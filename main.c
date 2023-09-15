4bceb9ec93930c6cd82bb813aff515484463b550
seono9y <144989211+seono9y@users.noreply.github.com>
2023-09-15 18:17:32 +0900

    프로그래밍2023 02_실습

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  exercise_02
+//
+//  Created by 추서연 on 2023/09/15.
+//
+
+#include <stdio.h>
+
+
+int main(int argc, char * argv[]) {
+    printf("Hello, Sookmyung Electronics!\n");
+    return 0;
+    
+}
